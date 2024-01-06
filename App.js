271895e752b6225b56929bd92d2a38501e1767e4
Pavan Pasumarthy <pavanpasumarthy2016@gmail.com>
2024-01-06 14:53:21 -0800

    React Hello World Program

---
+++ App.js
@@ -0,0 +1,16 @@
+const heading = React.createElement(
+    "h1",
+    {id:"heading",xyz:"abc"},
+    "Hello World from React!"
+);
+            
+
+const parent=React.createElement
+("div",
+{id:"Parent"},
+React.createElement("div",{id:"Child"},
+[React.createElement("h1",{},"Pavan Kumar Pasumarthy"),React.createElement("h1",{},"Srinivas Rao Pasumarthy")]));
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+            
+root.render(parent);
