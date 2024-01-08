ca372b6cc48eb8c844bbf5b6ffd619a7a8f2e2f7
Pavan Pasumarthy <pavanpasumarthy2016@gmail.com>
2024-01-07 18:05:40 -0800

    Functional Components & JSX

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
-const heading = React.createElement(
-    "h1",
-    {id:"heading",xyz:"abc"},
-    "Hello World from React!"
-);
-            
 
-const parent=React.createElement
-("div",
-{id:"Parent"},
-React.createElement("div",{id:"Child"},
-[React.createElement("h1",{},"Pavan Kumar Pasumarthy"),React.createElement("h1",{},"Srinivas Rao Pasumarthy")]));
+const Title=()=>
+    (<h1 id="heading" className='Pasumarthy'>
+    Pavan Kumar
+    </h1>
+    );
+            
+const HeadingComponent=()=>{
+    return(
+        <div>
+            <Title />
+            <h1 id="heading"></h1>
+        </div>
+    );
+}
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
             
-root.render(parent);
+root.render(<HeadingComponent />  ); 
+
