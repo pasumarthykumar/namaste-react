6e7e38d4b61cfc81dd9a9a7bc6ecb639d6f07567
Pavan Pasumarthy <pavanpasumarthy2016@gmail.com>
2024-01-15 14:26:44 -0800

    Explored the World with a Fetch API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,48 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body =()=>{
-    const [restaurantList,setRestaurantList]=useState(resList); 
-    return (
+    const [restaurantList,setRestaurantList]=useState([]); 
+    const [filteredList,setFilteredList]=useState([]);
+    const [searchText,setSearchText]=useState([]);
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+    
+    const fetchData =async () =>{
+        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING");
+        const json=await data.json();
+        setRestaurantList(json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        setFilteredList(json?.data?.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+    }
+
+    return filteredList.length === 0 ? (
+        <Shimmer />
+      ) : (
         <div className='body'>
+            <input placeholder="Search" value={searchText} onChange={(e)=>{
+                setSearchText(e.target.value);
+            }}></input>
             <button className="filter-btn" onClick={()=>{
-                const filteredList=resList.filter((restaurant)=>{
-                return restaurant.data.avgRating>4.0;
+                const filteredList=restaurantList.filter((restaurant)=>{
+                return restaurant.info.name.includes(searchText);
+
             });
-            setRestaurantList(filteredList);
-            }}>Filter Top Restaurants</button>
+            setFilteredList(filteredList);
+            }}>Search</button>
             <div className='res-container'>
             {
-                restaurantList.map((restaurant)=>{
-                    return <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
+                filteredList.map((restaurant)=>{
+                    return <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                 })
             }
             </div>
         </div>
     );
 }
+
 
 export default Body;
